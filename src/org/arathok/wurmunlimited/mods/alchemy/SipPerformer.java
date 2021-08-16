0aa50c5bac5b679bee89ffda58a294db81fe8861
Arathok <36503011+Arathok@users.noreply.github.com>
2021-08-16 15:14:00 +0200

    Spell?

--- src/org/arathok/wurmunlimited/mods/alchemy/SipPerformer.java
+++ src/org/arathok/wurmunlimited/mods/alchemy/SipPerformer.java
@@ -6,6 +6,12 @@ import com.wurmonline.server.behaviours.Actions;
 import com.wurmonline.server.creatures.Creature;
 import com.wurmonline.server.items.Item;
 import com.wurmonline.server.items.NotOwnedException;
+import com.wurmonline.server.Server;
+import com.wurmonline.server.creatures.Creature;
+import com.wurmonline.server.creatures.SpellEffects;
+import com.wurmonline.server.skills.Skill;
+import com.wurmonline.shared.constants.AttitudeConstants;
+import com.wurmonline.server.spells.*;
 
 //import net.bdew.wurm.halloween.Broom;
 import org.gotti.wurmunlimited.modsupport.actions.ActionPerformer;
@@ -17,8 +23,8 @@ public class SipPerformer implements ActionPerformer {
 		return Actions.USE;
 	}
 
-	public static boolean canUse(Creature performer, Item target) throws NotOwnedException {
-		return performer.isPlayer() && target.getOwner() == performer.getWurmId() && !target.isTraded();
+	public static boolean canUse(Creature performer, Item target) {
+		return performer.isPlayer() && target.getOwnerId() == performer.getWurmId() && !target.isTraded();
 	}
 
 	@Override
@@ -28,25 +34,51 @@ public class SipPerformer implements ActionPerformer {
 
 	@Override
 	public boolean action(Action action, Creature performer, Item target, short num, float counter) {
-		if (target.getTemplateId() != AlchItems.potionIdHeal)
+		if (target.getTemplateId() != AlchItems.potionIdTruehit)
 			return propagate(action, ActionPropagation.SERVER_PROPAGATION,
 					ActionPropagation.ACTION_PERFORMER_PROPAGATION);
 
-		try {
-			if (!canUse(performer, target)) {
-				performer.getCommunicator().sendAlertServerMessage("You are not allowed to do that");
-				return propagate(action, ActionPropagation.FINISH_ACTION, ActionPropagation.NO_SERVER_PROPAGATION,
-						ActionPropagation.NO_ACTION_PERFORMER_PROPAGATION);
-			}
-		} catch (NotOwnedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (!canUse(performer, target)) {
+			performer.getCommunicator().sendAlertServerMessage("You are not allowed to do that");
+			return propagate(action, ActionPropagation.FINISH_ACTION, ActionPropagation.NO_SERVER_PROPAGATION,
+					ActionPropagation.NO_ACTION_PERFORMER_PROPAGATION);
 		}
-		
-		// EFFECT STUFF GOES HERE
 
-		
-		//performer.consume(true);
+		if (target.getTemplateId() == AlchItems.potionIdTruehit)
+		// EFFECT STUFF GOES HERE
+		{
+			
+			
+			  
+				      
+			
+				float power=target.getCurrentQualityLevel();
+				 int seconds=300;
+			    Spell sp = Spells.getSpell(30);
+			    SpellEffects effs = performer.getSpellEffects();
+			   
+			    if (effs == null)
+				      effs = performer.createSpellEffects(); 
+			    
+			    SpellEffect eff = effs.getSpellEffect(sp.getEnchantment());
+			    
+			    if (eff==null)
+			    {
+				      eff = new SpellEffect(performer.getWurmId(), sp.getEnchantment(), power, Math.max(1, seconds));
+				      effs.addSpellEffect(eff);
+				    } 
+			    
+			    else if (eff.getPower() < power) {
+				      eff.setPower((float)power);
+				      eff.setTimeleft(Math.max(eff.timeleft, Math.max(1, seconds)));
+				      performer.sendUpdateSpellEffect(eff);
+				    } 
+		}
+			 
+			  
+			
+		}
+		// performer.consume(true);
 
 		return propagate(action, ActionPropagation.FINISH_ACTION, ActionPropagation.NO_SERVER_PROPAGATION,
 				ActionPropagation.NO_ACTION_PERFORMER_PROPAGATION);
