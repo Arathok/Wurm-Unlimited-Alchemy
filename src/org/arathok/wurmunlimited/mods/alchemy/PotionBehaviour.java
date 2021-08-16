58a192577d7265a44d6b747a86a376084e2c8755
Arathok <36503011+Arathok@users.noreply.github.com>
2021-08-16 17:52:38 +0200

    meh

--- src/org/arathok/wurmunlimited/mods/alchemy/PotionBehaviour.java
+++ src/org/arathok/wurmunlimited/mods/alchemy/PotionBehaviour.java
@@ -14,109 +14,74 @@ import java.util.List;
 
 public class PotionBehaviour implements BehaviourProvider {
 	private List<ActionEntry> consume;
-	
+
 	public PotionBehaviour() {
-        consume = Collections.singletonList(new ActionEntry(Actions.USE, "Consume", "consuming"));
-        
+		consume = Collections.singletonList(new ActionEntry(Actions.USE, "Consume", "consuming"));
+
 	}
+
 	@Override
 	public List<ActionEntry> getBehavioursFor(Creature performer, Item target) {
 		if (target.getTemplateId() == AlchItems.potionIdHeal) {
-			try {
-				if (SipPerformer.canUse(performer, target))
-					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
+			if (SipPerformer.canUse(performer, target))
+				return new ArrayList<>(consume);
+
 		} else if (target.getTemplateId() == AlchItems.potionIdGoat) {
-			try {
-				if (SipPerformer.canUse(performer, target))
-					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
+			if (SipPerformer.canUse(performer, target))
+				return new ArrayList<>(consume);
 		} else if (target.getTemplateId() == AlchItems.potionIdExcell) {
-			try {
+
+			if (SipPerformer.canUse(performer, target))
+				return new ArrayList<>(consume);
+	} 
+		else if (target.getTemplateId() == AlchItems.potionIdFranticCharge) {
+			
 				if (SipPerformer.canUse(performer, target))
 					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else if (target.getTemplateId() == AlchItems.potionIdFranticCharge) {
-			try {
-				if (SipPerformer.canUse(performer, target))
-					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		
 		} else if (target.getTemplateId() == AlchItems.potionIdMorningFog) {
-			try {
+			
 				if (SipPerformer.canUse(performer, target))
 					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 		} else if (target.getTemplateId() == AlchItems.potionIdOakshell) {
-			try {
+			
 				if (SipPerformer.canUse(performer, target))
 					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 		} else if (target.getTemplateId() == AlchItems.potionIdSixthSense) {
-			try {
+			
 				if (SipPerformer.canUse(performer, target))
 					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 		} else if (target.getTemplateId() == AlchItems.potionIdTruehit) {
-			try {
+			 List<ActionEntry> list = new ArrayList<>();
+			 list.add(Actions.actionEntrys[Actions.USE]);
 				if (SipPerformer.canUse(performer, target))
-					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+					return consume;
+			
 		} else if (target.getTemplateId() == AlchItems.potionIdStrength) {
-			try {
+			
 				if (SipPerformer.canUse(performer, target))
 					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 		} else if (target.getTemplateId() == AlchItems.potionIdWillowspine) {
-			try {
+			
 				if (SipPerformer.canUse(performer, target))
 					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		
 		} else if (target.getTemplateId() == AlchItems.potionIdVynora) {
-			try {
+			
 				if (SipPerformer.canUse(performer, target))
 					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 		} else if (target.getTemplateId() == AlchItems.potionIdRefresh) {
-			try {
+			
 				if (SipPerformer.canUse(performer, target))
 					return new ArrayList<>(consume);
-			} catch (NotOwnedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 		} else
 			return null;
 		return null;
