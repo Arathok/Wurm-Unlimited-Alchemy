7561f63cbfe9b9a0040d588a4053b555345e0729
Arathok <36503011+Arathok@users.noreply.github.com>
2021-08-16 18:00:05 +0200

    Add files via upload

--- src/org/arathok/wurmunlimited/mods/alchemy/Alchemy.java
+++ src/org/arathok/wurmunlimited/mods/alchemy/Alchemy.java
@@ -5,6 +5,17 @@ import com.wurmonline.server.items.*;
 import com.wurmonline.server.skills.SkillList;
 import com.wurmonline.shared.constants.IconConstants;
 
+import com.wurmonline.server.Servers;
+import com.wurmonline.server.creatures.Communicator;
+import javassist.ClassPool;
+import javassist.CtClass;
+import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
+import org.gotti.wurmunlimited.modloader.interfaces.*;
+
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import java.io.IOException;
 import java.util.Properties;
 
@@ -16,13 +27,29 @@ import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
 import org.gotti.wurmunlimited.modloader.interfaces.ServerStartedListener;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
 import org.gotti.wurmunlimited.modsupport.ItemTemplateBuilder;
+import org.gotti.wurmunlimited.modsupport.actions.ModActions;
 
 public class Alchemy implements WurmServerMod, Initable, PreInitable, Configurable, ServerStartedListener, ItemTemplatesCreatedListener, PlayerMessageListener{
 
 	@Override
 	public void preInit() {
-		// TODO Auto-generated method stub
 		
+		try
+		{
+		ClassPool classPool = HookManager.getInstance().getClassPool();
+		CtClass ctPlayers = classPool.getCtClass("com.wurmonline.server.Players");
+        ctPlayers.getMethod("sendStartKingdomChat", "(Lcom/wurmonline/server/players/Player;)V").setBody("return;");
+        ctPlayers.getMethod("sendStartGlobalKingdomChat", "(Lcom/wurmonline/server/players/Player;)V").setBody("return;");
+		
+		CtClass ctPlayer = classPool.getCtClass("com.wurmonline.server.players.Player");
+
+        ctPlayer.getMethod("isKingdomChat", "()Z").setBody("return false;");
+        ctPlayer.getMethod("isGlobalChat", "()Z").setBody("return false;");
+        ctPlayer.getMethod("seesPlayerAssistantWindow", "()Z").setBody("return false;");
+		 } catch (Throwable e) {
+	            throw new RuntimeException(e);
+		 }
+
 	}
 
 	@Override
@@ -50,7 +77,8 @@ public class Alchemy implements WurmServerMod, Initable, PreInitable, Configurab
 	@Override
 	public void onServerStarted() {
 		// TODO Auto-generated method stub
-		
+	      ModActions.registerActionPerformer(new SipPerformer());
+          ModActions.registerBehaviourProvider(new PotionBehaviour());
 	}
 
 	@Override
