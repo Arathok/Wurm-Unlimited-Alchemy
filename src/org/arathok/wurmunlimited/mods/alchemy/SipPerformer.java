dfb44707834d26bc65bfdae20a6c1895aefc3c54
Arathok <36503011+Arathok@users.noreply.github.com>
2021-08-17 15:33:03 +0200

    Get Killed by potion abuse!

--- src/org/arathok/wurmunlimited/mods/alchemy/SipPerformer.java
+++ src/org/arathok/wurmunlimited/mods/alchemy/SipPerformer.java
@@ -15,9 +15,17 @@ import com.wurmonline.server.spells.Spell;
 import com.wurmonline.server.spells.SpellEffect;
 import com.wurmonline.server.spells.Spells;
 
+import java.util.HashMap;
 import java.util.logging.Level;
 
 public class SipPerformer implements ActionPerformer {
+
+	int seconds = 300;
+	float power = 0;
+	int toxicity = 0;
+
+	HashMap<Long, Long> cooldown = new HashMap<Long, Long>();
+
 	@Override
 	public short getActionId() {
 		return Actions.DRINK;
@@ -29,46 +37,55 @@ public class SipPerformer implements ActionPerformer {
 
 	@Override
 	public boolean action(Action action, Creature performer, Item target, short num, float counter) {
-		Alchemy.logger.log(Level.INFO, "BLAH BLAH HE PERFORMS");
+		//Alchemy.logger.log(Level.INFO, "BLAH BLAH HE PERFORMS");
 		if (target.getTemplateId() != AlchItems.potionIdTruehit)
-			return propagate(action, ActionPropagation.SERVER_PROPAGATION,
+			return propagate(action,
+					ActionPropagation.SERVER_PROPAGATION,
 					ActionPropagation.ACTION_PERFORMER_PROPAGATION);
 
 		if (!canUse(performer, target)) {
 			performer.getCommunicator().sendAlertServerMessage("You are not allowed to do that");
-			return propagate(action, ActionPropagation.FINISH_ACTION, ActionPropagation.NO_SERVER_PROPAGATION,
+			return propagate(action,
+					ActionPropagation.FINISH_ACTION,
+					ActionPropagation.NO_SERVER_PROPAGATION,
 					ActionPropagation.NO_ACTION_PERFORMER_PROPAGATION);
+
 		}
 // EFFECT STUFF GOES HERE
-		if (target.getTemplateId() == AlchItems.potionIdTruehit)
+		if (target.getTemplateId() == AlchItems.potionIdTruehit) {
+			if (!cooldown.containsKey(performer.getWurmId()) || cooldown.get(performer.getWurmId()) + 300000 < System.currentTimeMillis()) {
+				power = target.getCurrentQualityLevel();
 
-		{
+				SpellEffects effs = performer.getSpellEffects();
 
-			float power = target.getCurrentQualityLevel();
-			int seconds = 300;
+				if (effs == null)
+					effs = performer.createSpellEffects();
 
-			SpellEffects effs = performer.getSpellEffects();
+				SpellEffect eff = effs.getSpellEffect((byte) 30);
 
-			if (effs == null)
-				effs = performer.createSpellEffects();
+				if (eff == null) {
+					eff = new SpellEffect(performer.getWurmId(), (byte) 30, power, Math.max(1, seconds));
+					effs.addSpellEffect(eff);
+				} else if (eff.getPower() < power) {
+					eff.setPower(power);
+					eff.setTimeleft(Math.max(eff.timeleft, Math.max(1, seconds)));
+					performer.sendUpdateSpellEffect(eff);
+				}
+				Items.destroyItem(target.getWurmId());
+				cooldown.put(performer.getWurmId(), System.currentTimeMillis());
+				toxicity = 0;
+			} else if (toxicity < 1) {
+				performer.getCommunicator().sendAlertServerMessage("You are still under influence of another potion! " +
+						"Drinking another one would probably kill you because of toxicity");
 
-			SpellEffect eff = effs.getSpellEffect((byte) 30);
-
-			if (eff == null) {
-				eff = new SpellEffect(performer.getWurmId(), (byte) 30, power, Math.max(1, seconds));
-				effs.addSpellEffect(eff);
+				toxicity++;
+			} else if (toxicity > 0) {
+				performer.destroy();
+				performer.getCommunicator().sendAlertServerMessage("You feel the rush of alchemical power in every nerve of your body, " +
+						"only for the feeling of power to subside after a short while" +
+						" and your body collapses under the toxins.");
 			}
-
-			else if (eff.getPower() < power) {
-				eff.setPower((float) power);
-				eff.setTimeleft(Math.max(eff.timeleft, Math.max(1, seconds)));
-				performer.sendUpdateSpellEffect(eff);
-			}
-			Items.destroyItem(target.getWurmId());
 		}
-
-		// performer.consume(true);
-
 		return propagate(action, ActionPropagation.FINISH_ACTION, ActionPropagation.NO_SERVER_PROPAGATION,
 				ActionPropagation.NO_ACTION_PERFORMER_PROPAGATION);
 	}
