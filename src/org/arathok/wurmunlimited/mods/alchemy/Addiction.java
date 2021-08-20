4f0552c4f8278f3257167980818f3cb429ad2c0c
Arathok <36503011+Arathok@users.noreply.github.com>
2021-08-20 14:27:06 +0200

    Add files via upload

--- src/org/arathok/wurmunlimited/mods/alchemy/Addiction.java
+++ src/org/arathok/wurmunlimited/mods/alchemy/Addiction.java
@@ -7,6 +7,7 @@ import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
 import org.gotti.wurmunlimited.modloader.interfaces.*;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,11 +26,44 @@ import com.wurmonline.server.Items;
 import com.wurmonline.server.behaviours.Action;
 import com.wurmonline.server.behaviours.Actions;
 import com.wurmonline.server.creatures.Creature;
+import com.wurmonline.server.creatures.*;
 import com.wurmonline.server.creatures.SpellEffects;
 import com.wurmonline.server.items.Item;
+import com.wurmonline.server.players.Player;
+import com.wurmonline.server.Players;
 import com.wurmonline.server.spells.SpellEffect;
 import org.gotti.wurmunlimited.modsupport.actions.ActionPerformer;
 import org.gotti.wurmunlimited.modsupport.actions.ActionPropagation;
 
+public class Addiction {
+	
+	
+	public void addictionHandler()
+	{
+		
+		for (Entry<Long, Integer> set : Alchemy.addiction.entrySet())
+		{
+			Creature player;
+			long wurmId= set.getKey();
+			int x=set.getValue();
+			SpellEffect eff;
+			player = Players.getPlayerById(wurmId);
+			if (x > 10)
+			{
+			
+				float power = x;
 
+				
+
+				
+				
+			}
+			
+			
+			
+		}
+		
+	}
 }
+
+
