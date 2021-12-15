d42d6aaee5a07b9de1a04862fe370e4c147a328e
Arathok <36503011+Arathok@users.noreply.github.com>
2021-12-15 14:24:14 +0100

    Oil self deletion

--- src/org/arathok/wurmunlimited/mods/alchemy/Enchantments.java
+++ src/org/arathok/wurmunlimited/mods/alchemy/Enchantments.java
@@ -8,6 +8,7 @@ import com.wurmonline.server.items.*;
 import com.wurmonline.server.players.Player;
 import com.wurmonline.server.spells.SpellEffect;
 
+import java.util.Iterator;
 import java.util.Map;
 
 public class Enchantments {
@@ -19,49 +20,50 @@ public class Enchantments {
     public static void EnchantmentHandler() throws NoSuchItemException {
         long time = System.currentTimeMillis();
 
-        for (Map.Entry<Long, Long> weapons : Alchemy.weaponsWithOils.entrySet()) {
-            if (weapons.getValue() < time) {
-
-                wurmId = weapons.getKey();
+        Iterator<Map.Entry<Long, Long>> weapons = Alchemy.weaponsWithOils.entrySet().iterator();
+        Iterator<Map.Entry<Long, String>> enchants = Alchemy.weaponsWithOilsEnchants.entrySet().iterator();
+        while (weapons.hasNext()) {
+            if (weapons.next().getValue() < time) {
+                wurmId = weapons.next().getKey();
                 if (wurmId != null) {
                     if (!Config.enchantmentsStack) {
                         i = Items.getItem(wurmId);
                         p = Players.getInstance().getPlayerOrNull(i.getOwnerId());
-                        if (time > weapons.getValue()) {
+                        if (time > weapons.next().getValue()) {
                             ItemSpellEffects effs = i.getSpellEffects();
                             SpellEffect[] speffs = effs.getEffects();
                             if (speffs.length > 0) {
                                 effs.removeSpellEffect((speffs[0]).type);
                             }
-
                             p.getCommunicator().sendAlertServerMessage("The oil dried completely off your " + i.getName()
                                     + " and it goes back to a normal " + i.getTemplate().getName() + ".");
                             i.setName(i.getTemplate().getName());
-                            Alchemy.weaponsWithOils.remove(wurmId);
+                            weapons.remove();
                         }
 
                     } else // hier bei gestackten einfügen
-                        for (Map.Entry<Long, String> enchants : Alchemy.weaponsWithOilsEnchants.entrySet()) {
+                        while(enchants.hasNext()){
                             i = Items.getItem(wurmId);
                             p = Players.getInstance().getPlayerOrNull(i.getOwnerId());
-                            if (time > weapons.getValue()) {
-                                ItemSpellEffects itemEffects = i.getSpellEffects();
-                                SpellEffect[] spellsOnItems = itemEffects.getEffects();
-
-                                for (SpellEffect spell : spellsOnItems) {
-                                    if (spell.getName().equals(enchants.getValue())) {
-                                        itemEffects.removeSpellEffect(spell.type);
-                                    }
+                            if (time > weapons.next().getValue()) {
+                            ItemSpellEffects itemEffects = i.getSpellEffects();
+                            SpellEffect[] spellsOnItems = itemEffects.getEffects();
+                            for (SpellEffect spell : spellsOnItems) {
+                                if (spell.getName().equals(enchants.next().getValue())) {
+                                    itemEffects.removeSpellEffect(spell.type);
                                 }
-                                Alchemy.weaponsWithOils.remove(wurmId);
-                                Alchemy.weaponsWithOilsEnchants.remove(wurmId);
+                                p.getCommunicator().sendAlertServerMessage("The oil dried completely off your " + i.getName()
+                                        + " and it goes back to how it was before " + i.getTemplate().getName() + ".");
+                                i.setName(i.getTemplate().getName());
                             }
+                            enchants.remove();
 
-                   }
+                        }
+                    }
                 }
+
             }
         }
     }
 }
-
 
