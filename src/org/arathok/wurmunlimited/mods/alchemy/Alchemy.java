c094cefabb2de2358ce5ca3a9b6c3ad29d735464
Arathok <36503011+Arathok@users.noreply.github.com>
2023-01-26 15:08:04 +0100

    config

--- src/org/arathok/wurmunlimited/mods/alchemy/Alchemy.java
+++ src/org/arathok/wurmunlimited/mods/alchemy/Alchemy.java
@@ -52,7 +52,7 @@ public class Alchemy implements WurmServerMod, Initable, PreInitable, Configurab
 		Config.oilDuration = Integer.parseInt(properties.getProperty("oilDuration", "300"));
 		Config.cooldownPotion = Integer.parseInt(properties.getProperty("cooldownPotion", "300"));
 		Config.cooldownHeal = Integer.parseInt(properties.getProperty("cooldownHeal", "300"));
-		Config.cooldownUltimate = Integer.parseInt(properties.getProperty("cooldownUltimate", "3600"));
+
 		Config.purifiedWaterCooking = Boolean.parseBoolean(properties.getProperty("purifiedWaterCooking", "true"));
 		Config.addictionTimer = Integer.parseInt(properties.getProperty("addictionTimer", "900"));
 		Config.enchantmentsStack = Boolean.parseBoolean(properties.getProperty("enchantmentsStack", "true"));
