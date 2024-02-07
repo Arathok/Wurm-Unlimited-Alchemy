4ea5dd9b601e09fdc9c5900d934336c05e03dead
Arathok <36503011+Arathok@users.noreply.github.com>
2024-02-07 18:30:18 +0100

    TEst

--- src/main/java/org/arathok/wurmunlimited/mods/alchemy/enchantments/Enchantment.java
+++ src/main/java/org/arathok/wurmunlimited/mods/alchemy/enchantments/Enchantment.java
@@ -1,34 +1,40 @@
 package org.arathok.wurmunlimited.mods.alchemy.enchantments;
 
+
+import org.arathok.wurmunlimited.mods.alchemy.Alchemy;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 
-import org.arathok.wurmunlimited.mods.alchemy.Alchemy;
-
 public class Enchantment {
 
-    public long itemId = 0L;
-    public long timeOfEnchantment = 0;
-    public byte enchantmentType = 0;
-    public boolean hasOil = false;
-    public long playerId = 0;
-    public String itemNameBeforeEnchantment = "";
 
-    public static void readFromSQL(Connection dbconn) throws SQLException {
-        Enchantment e = new Enchantment();
+
+    public long itemId=0L;
+    public long timeOfEnchantment=0;
+    public byte enchantmentType=0;
+    public boolean hasOil=false;
+    public long playerId = 0;
+    public String itemNameBeforeEnchantment="";
+
+
+ public static void readFromSQL(Connection dbconn) throws SQLException {
+
         PreparedStatement ps = dbconn.prepareStatement("SELECT * FROM Alchemy_OiledWeapons");
-        ResultSet rs = ps.executeQuery();
+        ResultSet rs=ps.executeQuery();
         while (rs.next()) {
 
+
+            Enchantment e=new Enchantment();
             e.itemId = rs.getLong("itemId"); // liest quasi den Wert von der Spalte
             e.timeOfEnchantment = rs.getLong("timeOfEnchantment");
             e.enchantmentType = rs.getByte("enchantmentType");
             e.hasOil = rs.getBoolean("hasOil");
             e.playerId = rs.getLong("playerId");
-            e.itemNameBeforeEnchantment = rs.getString("itemNameBeforeEnchantment");
+            e.itemNameBeforeEnchantment=rs.getString("itemNameBeforeEnchantment");
             EnchantmentHandler.enchantments.add(e);
 
         }
@@ -37,19 +43,22 @@ public class Enchantment {
 
     public void insert(Connection dbconn) throws SQLException {
 
-        PreparedStatement ps = dbconn.prepareStatement(
-                "insert into Alchemy_OiledWeapons (itemID,playerId,timeOfEnchantment,enchantmentType,hasOil,itemNameBeforeEnchantment) "
-                        + "values " + "(?,?,?,?,?,?)");
-        ps.setLong(1, this.itemId);
+        PreparedStatement ps = dbconn.prepareStatement("insert into Alchemy_OiledWeapons (itemID,playerId,timeOfEnchantment,enchantmentType,hasOil,itemNameBeforeEnchantment) " +
+                                                               "values " +
+                                                               "(?,?,?,?,?,?)");
+        ps.setLong(1,this.itemId);
         ps.setLong(2, this.playerId);
         ps.setLong(3, this.timeOfEnchantment);
         ps.setByte(4, this.enchantmentType);
-        ps.setBoolean(5, this.hasOil);
-        ps.setString(6, this.itemNameBeforeEnchantment);
+        ps.setBoolean(5,this.hasOil);
+        ps.setString(6,this.itemNameBeforeEnchantment);
 
         ps.executeUpdate();
 
+
+
     }
+
 
     public static void remove(Connection dbconn, long aItemId) {
         try {
@@ -57,12 +66,15 @@ public class Enchantment {
             ps.setLong(1, aItemId);
             ps.execute();
             ps.close();
-        }
-        catch (SQLException throwables) {
-            Alchemy.logger.log(Level.SEVERE, "something went wrong deleting an enchanted Item from the DB!",
-                    throwables);
+        } catch (SQLException throwables) {
+            Alchemy.logger.log(Level.SEVERE, "something went wrong deleting an enchanted Item from the DB!", throwables);
             throwables.printStackTrace();
         }
     }
+
+
+
+
+
 
 }
